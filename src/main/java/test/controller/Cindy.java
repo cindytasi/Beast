d31243cc326ad7874e87926e41ea9dea4f7bbf1e
cindy <cindy535588@gmail.com>
2023-07-03 16:02:04 +0800

    msg to teacher

--- src/main/java/test/controller/Cindy.java
+++ src/main/java/test/controller/Cindy.java
@@ -2,6 +2,6 @@ package test.controller;
 
 public class Cindy {
 	public static void main(String[] args) {
-		System.out.println("jason is monster");
+		System.out.println("老師教我們servlet~please~~~");
 	}
 }
