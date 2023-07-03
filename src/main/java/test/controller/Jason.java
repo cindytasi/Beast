04908b9c35f9d407e8a2d5ee48721208cb7db67b
Jason <eminem9155@gmail.com>
2023-07-03 16:01:47 +0800

    ＄. $

--- src/main/java/test/controller/Jason.java
+++ src/main/java/test/controller/Jason.java
@@ -2,6 +2,6 @@ package test.controller;
 
 public class Jason {
 	public static void main(String[] args) {
-		System.out.println("吼齁～cindy is beast!!");
+		System.out.println("吼齁～cindy is beast!!嘻嘻");
 	}
 }
